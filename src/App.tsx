6c549fe223941e012b14a78e04de6d4b1b3010b7
mufeiyu-ayu <2382839439@qq.com>
2024-04-23 07:23:15 +0800

    state交互初步学习

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 // import ObjectState from './component/State/object'
-import ArrayState from './component/State/array.tsx'
+// import ArrayState from './component/State/array.tsx'
+import Reactive from './component/statemanger/reactive'
 export default function App() {
   return (
     <>
-      <ArrayState />
+      <Reactive />
     </>
   )
 }
--- src/component/State/array.tsx
+++ src/component/State/array.tsx
@@ -1,38 +1,58 @@
 import { useState } from 'react'
-let nextIndex = 0
-type Arr = {
-  id: number
-  name: string
-}
-export default function ArrayState() {
-  const [name, setName] = useState('')
-  const [artists, setArtists] = useState<Arr[]>([])
+
+let nextId = 3
+const initialList = [
+  { id: 0, title: 'Big Bellies', seen: false },
+  { id: 1, title: 'Lunar Landscape', seen: false },
+  { id: 2, title: 'Terracotta Army', seen: true }
+]
+
+export default function BucketList() {
+  const [myList, setMyList] = useState(initialList)
+  const [yourList, setYourList] = useState(initialList)
+
+  function handleToggleMyList(artworkId, nextSeen) {
+    const myNextList = [...myList]
+    const artwork = myNextList.find((a) => a.id === artworkId)
+    artwork.seen = nextSeen
+    setMyList(myNextList)
+  }
+
+  function handleToggleYourList(artworkId, nextSeen) {
+    const yourNextList = [...yourList]
+    const artwork = yourNextList.find((a) => a.id === artworkId)
+    artwork.seen = nextSeen
+    setYourList(yourNextList)
+  }
 
   return (
     <>
-      <h1>晚上没睡觉的人：</h1>
-      <div>
-        <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
-        <button
-          onClick={() => {
-            setArtists([
-              ...artists,
-              {
-                id: nextIndex++,
-                name: name
-              }
-            ])
-            setName('')
-          }}
-        >
-          添加
-        </button>
-        <ul>
-          {artists.map(({ id, name }) => (
-            <li key={id}>{name}</li>
-          ))}
-        </ul>
-      </div>
+      <h1>艺术愿望清单</h1>
+      <h2>我想看的艺术清单：</h2>
+      <ItemList artworks={myList} onToggle={handleToggleMyList} />
+      <h2>你想看的艺术清单：</h2>
+      <ItemList artworks={yourList} onToggle={handleToggleYourList} />
     </>
+  )
+}
+
+function ItemList({ artworks, onToggle }) {
+  return (
+    <ul>
+      {artworks.map((artwork) => (
+        <li key={artwork.id}>
+          <label>
+            <input
+              type='checkbox'
+              checked={artwork.seen}
+              onChange={(e) => {
+                onToggle(artwork.id, e.target.checked)
+              }}
+            />
+            {artwork.title}
+          </label>
+        </li>
+      ))}
+    </ul>
   )
 }
